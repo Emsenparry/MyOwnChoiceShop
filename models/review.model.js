06987836d4a01935819e21ee8db8876c028685ec
Emma Nielsen <Emmanielsen843@gmail.com>
2022-12-13 10:45:02 +0100

    review model

---
+++ models/review.model.js
@@ -0,0 +1,40 @@
+import sequelize from '../config/sequelize.config.js'
+import { DataTypes, Model } from 'sequelize'
+
+class ReviewModel extends Model{}
+
+ReviewModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    sequelize, 
+    modelName: 'review',
+    freezeTableName: true,
+    underscored: true
+})
+
+export default ReviewModel
