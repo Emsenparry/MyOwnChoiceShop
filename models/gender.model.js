14c600d32465cefab54e70a3cf9d4f2d6229efd5
Emma Nielsen <Emmanielsen843@gmail.com>
2022-12-13 10:52:15 +0100

    Rest of the models created

---
+++ models/gender.model.js
@@ -0,0 +1,24 @@
+import sequelize from '../config/sequelize.config.js'
+import { DataTypes, Model } from 'sequelize'
+
+class GenderModel extends Model{}
+
+GenderModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    gender: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize, 
+    modelName: 'gender',
+    freezeTableName: true,
+    underscored: true
+})
+
+export default GenderModel
---
+++ models/type.model.js
@@ -0,0 +1,24 @@
+import sequelize from '../config/sequelize.config.js'
+import { DataTypes, Model } from 'sequelize'
+
+class TypeModel extends Model{}
+
+TypeModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize, 
+    modelName: 'type',
+    freezeTableName: true,
+    underscored: true
+})
+
+export default TypeModel
