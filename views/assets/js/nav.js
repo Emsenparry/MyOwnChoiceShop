2129515e538c0a0b17975ccaa25ef1864bced060
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-03 12:30:12 +0100

    changes

---
+++ views/assets/js/nav.js
@@ -0,0 +1,9 @@
+const ArrContent = [
+    {title: 'products', content: 'lorem ipsum'}, 
+    {title: 'om os', content: 'lorem ipsum2'},
+    {title: 'login', content: 'lorem ipsum3'} 
+]
+
+function Link(pageID) {
+    console.log(ArrContent[pageID]);
+}
